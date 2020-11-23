5193c5e95eae85f8e7fd1a195019e6e497c1b3fd
anirudhsing <anirudhsingh.cdac@gmail.com>
2020-11-23 20:26:36 +0530

    Add files via upload

---
+++ vault.cmd
@@ -0,0 +1,7 @@
+this is secure lineinfile
+# ansible-vault encrypt vault.yml --to encrypt the file
+#
+# # ansible-vault view vault.yml -- to view the  secure file
+# # ansible-vault edit vault.yml -- to edit the  secure file
+#  ansible-vault decrypt vault.yml -- to decrypt the  secure file
+# #  ansible-vault rekey vault.yml -- to reset the  secure file
